ccd587924e370eb173953ab48b2ffb4eacb504b2
lidwa21 <88238724+lidwa21@users.noreply.github.com>
2021-08-01 16:56:51 +0200

    Test Walid Taleb
    Vous pouvez trouver le fichier exercice.py complété ainsi que le fichier Main_Walid_Taleb.ipynb contenant les executions des fonctions avec quelques commentaires.

--- exercise.py
+++ exercise.py
@@ -9,7 +9,11 @@ def check_uniqueness(lst):
     Check if a list contains only unique values.
     Returns True only if all values in the list are unique, False otherwise
     """
-    pass
+    nb_unique_elements  = len(set(lst)) # we get the number of unique values
+    if nb_unique_elements == len(lst):
+        return True
+    else :
+        return False
 
 
 def smallest_difference(array):
@@ -19,7 +23,13 @@ def smallest_difference(array):
     Please note that the array can be large and that the more
     computationally efficient the better
     """
-    pass
+    
+    flat_array = array.flatten() # we flatten the array to have a (n,) dimension
+    flat_array = np.array(sorted(flat_array)) # we sort the flat array in ascending order and make sure the result is an array 
+    
+    consecutive_differences = flat_array[1:] - flat_array[:-1] # we calculate the differences of side by side elements 
+    
+    return(min(consecutive_differences))
 
 
 # Finance and DataFrame manipulation
@@ -33,7 +43,13 @@ def macd(prices, window_short=12, window_long=26):
     Assume simple moving average rather than exponential moving average
     The expected output is in the output.csv file
     """
-    pass
+    numerical_col_names = prices.select_dtypes(include=np.number).columns # we select only the cumerical columns
+    df_result = pd.DataFrame()
+    for col_name in numerical_col_names:
+        df_result["MACD "+col_name] = prices[col_name].rolling(window=window_short).mean() - prices[col_name].rolling(window=window_long).mean()
+    
+    return df_result
+    
 
 
 def sortino_ratio(prices):
@@ -43,7 +59,23 @@ def sortino_ratio(prices):
     Assume risk-free rate = 0
     On the given test set, it should yield 0.05457
     """
-    pass
+    risk_free_rate = 0
+    prices = prices.select_dtypes(include=np.number) # we select only numerical columns
+    returns = prices.pct_change() # the calculate the returns for all columns
+    returns = returns.dropna()
+    
+    def downside_deviation(x):
+        return np.std(np.minimum(0,x))
+    
+    def daily_retun_2_annual(x):
+        cumulative_return = (x+1).prod()
+        ann_return = cumulative_return**(252/len(x)) - 1 # 252 number of open days in a year
+        return ann_return
+    
+    result = returns.apply(lambda x: (daily_retun_2_annual(x) - risk_free_rate)/downside_deviation(x), axis=0) # we calculathe the ratio
+    #################### Important 
+    ##  The resulting values is not the one expected. I must have implemented a wrong formula for the downside deviation. It might also be due to considering annual returns
+    return result
 
 
 def expected_shortfall(prices, level=0.95):
@@ -51,16 +83,29 @@ def expected_shortfall(prices, level=0.95):
     Code a function that takes a DataFrame named prices and
     returns the expected shortfall at a given level
     On the given test set, it should yield -0.03468
-    """
-    pass
+    """ 
+    prices = prices.select_dtypes(include=np.number) # we select only the cumerical columns
+    returns = prices.pct_change() # we calculate the returns for each column
+    returns = returns.dropna()
+    n_col = prices.shape[1]
+    ES = np.zeros(n_col)
+    for i in range(n_col): # we calculate the ES for each column
+        selected_column = returns.iloc[:,i]
+        worst_returns = selected_column[selected_column < selected_column.quantile(1-level)]
+        ES[i] = np.mean(worst_returns)
+    return ES
 
 
 # Plot
 
 
-def visualize(prices, path):
+def visualize(prices, path, plot_name = "SX5T Index", plot_type = ".pdf"):
     """
     Code a function that takes a DataFrame named prices and
     saves the plot to the given path
     """
-    pass
+    
+    prices_plot = prices.select_dtypes(include=np.number) # we select only the cumerical columns
+    prices_plot.plot(figsize=(8,6))
+    plt.legend()
+    plt.savefig(path+"/"+plot_name+plot_type)
